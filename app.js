1a4a7da4ecd1988a7ba9c9ec05608a2fbb0406bf
devlight97 <41066651+devlight97@users.noreply.github.com>
2020-03-03 09:27:29 +0700

    delete collection realtime firebase

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ function renderCafe(doc) {
 	let li = document.createElement('li');
 	let name = document.createElement('span');
 	let city = document.createElement('span');
+	let cross = document.createElement('div');
 
 	li.setAttribute('data-id', doc.id);
 	name.textContent = doc.data().name;
 	city.textContent = doc.data().city;
+	cross.textContent = 'x';
 
 	li.appendChild(name);
 	li.appendChild(city);
+	li.appendChild(cross);
 
 	cafeList.appendChild(li);
+
+	cross.addEventListener('click', e => {
+		e.stopPropagation();
+		let id = e.target.parentElement.getAttribute('data-id');
+		db.collection('cafes').doc(id).delete();
+	});
 }
 
 db.collection('cafes').get().then(snapshot => {
@@ -28,4 +37,6 @@ form.addEventListener('submit', e => {
 		name: form.name.value,
 		city: form.city.value
 	});
+	form.name.value = '';
+	form.city.value = '';
 })
